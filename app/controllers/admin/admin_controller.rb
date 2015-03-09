a6bc8c8628137b9ee55f2ca303c00970948e2e81
Julio Castillo <julio.castillo@tektonlabs.com>
2015-03-09 13:51:31 -0500

    removing old project

--- app/controllers/admin/admin_controller.rb
+++
@@ -1,7 +0,0 @@
-class Admin::AdminController < ApplicationController
-  layout "admin/admin"
-  before_action :authenticate_admin!
-
-  def index
-  end
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_tekton_rails_bundle_session'
